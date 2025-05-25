7fee35a05ebe1f4a265315e75b2db4c4ca9bdc42
Geoff Hill <geoffhill@protonmail.com>
2025-05-24 22:46:20 -0400

    push

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -2,7 +2,7 @@ import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import { CaretDownIcon } from "@radix-ui/react-icons";
 import { Link } from "react-router-dom";
 // import classNames from "classnames";
-import "./NavBar.css";
+import "./navbar.css"; 
 
 const NavBar = () => {
   return (
