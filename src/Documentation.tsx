e720e11dbdc981e3504248180581bb34ff6d8d7b
Geoff Hill <geoffhill@protonmail.com>
2025-05-24 21:53:40 -0400

    add video

--- src/Documentation.tsx
+++ src/Documentation.tsx
@@ -79,6 +79,33 @@ export default function Documentation() {
                     only need to change it again if you have a specific reason
                     to do so.
                   </p>
+                  <h2>Or check out this video for step by step instructions</h2>
+                  <Box
+                    style={{
+                      position: "relative",
+                      paddingBottom: "56.25%",
+                      height: 0,
+                      marginBottom: "1rem",
+                    }}
+                  >
+                    <iframe
+                      src="https://www.loom.com/embed/f2c42b126b004150b1e7a551fde466e6?sid=b5d8af47-31d7-4660-ace0-b0e31a3647c9"
+                      frameBorder="0"
+                      allowFullScreen
+                      style={{
+                        position: "absolute",
+                        top: 0,
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        borderRadius: "8px",
+                      }}
+                      title="WebGA Download and Installation Video"
+                    />
+                  </Box>
+             
+                        
+                 
                 </Accordion.Content>
               </Accordion.Item>
 
