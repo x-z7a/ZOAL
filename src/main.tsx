6995b3dbc8453a4a2414a8766160f0235d0cd99a
Geoff Hill <geoffhill@protonmail.com>
2025-05-24 21:19:22 -0400

    add documentation

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import Instructions from "./Instructions.tsx";
+import Documentation from "./Documentation.tsx";
 import Privacy from "./Privacy";
 import Terms from "./Terms";
 import ScrollToTop from "./ScrollToTop..tsx";
@@ -26,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/instructions" element={<Instructions />} />
+          <Route path="/documentation" element={<Documentation />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/contact" element={<Contact />} />
